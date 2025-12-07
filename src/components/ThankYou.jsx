327c6d82e50b39bb701401d496c4700706ecffee
Apple <apple@Apples-MacBook-Pro.local>
2025-12-07 19:53:04 +0530

    gtm configurations

--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./ThankYou.css";
 
 export default function ThankYou() {
+
+  useEffect(() => {
+    // Fire Meta Purchase Event
+    if (window.fbq) {
+      window.fbq("track", "Purchase", {
+        value: 99,     // Change to your price if needed
+        currency: "INR"
+      });
+    }
+  }, []);
+
   return (
     <div className="thankyou-wrapper">
       <h1 className="thankyou-title">Thank You!</h1>
